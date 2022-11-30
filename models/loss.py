6779193560a2f949751bdaacea1dd4b4e00eced7
ZigonK <zthucnguyen99@gmail.com>
2022-12-01 00:29:03 +0700

    fix: pseudo label implementation

--- models/loss.py
+++ models/loss.py
@@ -15,8 +15,8 @@ class SSLTLoss(nn.Module):
   
   def loss_ce(self, preds):
     bs, k, _ = preds.shape
-    # Create pseudo-label: ith cluster belongs to class i [bs x k x k]
-    tgt = F.one_hot(torch.arange(k).repeat(bs), k)
+    # Create pseudo-label: ith cluster belongs to class i [B, K, K]
+    tgt = F.one_hot(torch.arange(k).unsqueeze(0).repeat(bs, 1), k)
     loss_ce = F.cross_entropy(preds, tgt)
     return loss_ce
   def loss_contrast(self, preds):

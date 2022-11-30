680c2f58f160b667bb93c99f3e9c9d7eb31da0ed
ZigonK <zthucnguyen99@gmail.com>
2022-12-01 00:31:05 +0700

    verbose: debug

--- models/SSLTNet.py
+++ models/SSLTNet.py
@@ -31,7 +31,7 @@ class SSLTNet(nn.Module):
         features = self.backbone(inputs)
         cluster_prototypes = self.decoder(features)
         pred_logits = self.softmax(self.class_embed(cluster_prototypes))
-
+        print(pred_logits.size())
         if self.training:
             return self.criterion(pred_logits, features)
 

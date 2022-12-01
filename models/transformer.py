4e99e0ea368ab0012d0e4d633e84cf2e53576791
ZigonK <zthucnguyen99@gmail.com>
2022-12-01 23:22:43 +0700

    debug

--- models/transformer.py
+++ models/transformer.py
@@ -279,8 +279,8 @@ class SSLTransformerDecoder(nn.Module):
         # [B*h, Q, Q]
         self_attn_mask = multihead_attn_mask.unsqueeze(2).repeat(1, 1, 1, self.nqueries).flatten(0, 1)
 
-        print(cross_attn_mask)
-        print(self_attn_mask)
+        print(cross_attn_mask.shape)
+        print(self_attn_mask.shape)
         for i in range(self.num_layers):
             # attention: cross-attention first
             is_skip_connection = (i == 0) # here we prevent the model use the clusters directly for class prediction

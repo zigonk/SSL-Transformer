7658cb6d600aa8dc6cfe74f42bad3591f4b8d784
ZigonK <zthucnguyen99@gmail.com>
2022-12-05 08:09:26 +0700

    verbose clustering

--- inference_cam.py
+++ inference_cam.py
@@ -83,15 +83,16 @@ def inference(model, args):
         ncols = int(math.sqrt(q + 1))
 
         if (nrows * ncols < q + 1): nrows += 1
-        plt.clf()
         for j, cam in enumerate(CAM_batch):
+            plt.clf()
             plt.subplot(nrows, ncols, 1)
+            plt.axis('off')
             plt.imshow(img)
             for idx, am in enumerate(cam):
                 plt.subplot(nrows, ncols, idx + 2)
                 plt.axis('off')
                 plt.imshow(am)
-            plt.savefig(f"{args.output_dir}/visualize_cam/{i * args.batch_size + j:05d}.jpg")
+            plt.savefig(f"{args.output_dir}/visualize_cam/{i:05d}.jpg")
             
 def main(args):
 
--- models/clustering.py
+++ models/clustering.py
@@ -92,7 +92,7 @@ def cluster(features, num_cluster):
     nsample, d = features.shape
     niter = int(20 * (nsample / (1000 * num_cluster))) # As a rule of thumb there is no consistent improvement of the k-means quantizer beyond 20 iterations and 1000 * k training points
     # faiss implementation of k-means
-    clus = faiss.Kmeans(d, num_cluster, gpu=True, spherical=True, seed=42, niter=niter)
+    clus = faiss.Kmeans(d, num_cluster, gpu=True, spherical=True, seed=42, niter=niter, verbose=True)
 
     # Change faiss seed at each k-means so that the randomly picked
     # initialization centroids do not correspond to the same feature ids

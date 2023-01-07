fix softmax dim

models/SSLTNet.py
@@ -30,7 +30,7 @@ class SSLTNet(nn.Module):
         """
         features = self.backbone(inputs)
         if self.training:
-            k = 0.5
+            k = 1
         else:
             k = 0
         cluster_prototypes = self.decoder(features, k)

models/transformer.py
@@ -192,7 +192,7 @@ class CrossAttentionDecoder(nn.Module):
         k = int(outputs_mask.size(dim=softmax_dim) * k_ratio)
         attn_mask = self.get_topk_attention_mask(outputs_mask, k, softmax_dim)
 
-        outputs_mask = F.softmax(outputs_mask + attn_mask, dim=2)
+        outputs_mask = F.softmax(outputs_mask + attn_mask, dim=softmax_dim)
 
         updated_cluster_prototypes = torch.einsum(
             "bqf,bcf->bqc", outputs_mask, input_features)